b70dcf950d07d547a0b005893bd3c63e4451c5da
ishaan-puniani <ishaan.puniani@gmail.com>
2018-11-09 00:27:52 +0100

    skip linting rule

--- src/EmTrendz_FPS.ts
+++ src/EmTrendz_FPS.ts
@@ -1,7 +1,4 @@
-// Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
-// import "core-js/fn/array.find"
-// ...
-
+/* tslint:disable */
 import calculator from './calculator/index';
 export default class EmTrendz_FPS {
   constructor() {
